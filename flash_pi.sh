04367988c5f3ebb92c55e7a0a0a8bacba9381186
Ambroise Curutchague <ambroisecuru@gmail.com>
2025-11-21 11:29:43 +0200

    WIP unable to access Pi.

--- flash_pi.sh
+++ flash_pi.sh
@@ -1,5 +1,11 @@
+while getopts "d:" opt; do
+  case $opt in
+    d) DISK_NAME=$OPTARG ;;
+  esac
+done
 
-diskutil unmountDisk $(DISK_NAME)
-sudo dd if=/Users/melchizedek/Documents/projects/zambia/network_and_internet/openwrt-builder/bin/openwrt-24.10.1-bcm27xx-bcm2711-rpi-4-ext4-factory.img of=$(DISK_NAME) bs=4M status=progress conv=sync
+gunzip 
+diskutil unmountDisk $DISK_NAME
+sudo dd if=$PWD/bin/openwrt-24.10.1-bcm27xx-bcm2711-rpi-4-ext4-factory.img of=$DISK_NAME bs=4M status=progress conv=sync
 sync
-diskutil eject $(DISK_NAME)
+diskutil eject $DISK_NAME
